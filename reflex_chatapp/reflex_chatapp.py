a14ee3bd885da5277310df2c409dab6c22fb831b
arielbk <buchwald.kearney@gmail.com>
2023-08-06 12:26:01 +0200

    chore: initial scaffolding

---
+++ reflex_chatapp/reflex_chatapp.py
@@ -0,0 +1,31 @@
+from rxconfig import config
+
+import reflex as rx
+
+docs_url = "https://reflex.dev/docs/getting-started/introduction"
+filename = f"{config.app_name}/{config.app_name}.py"
+
+
+class State(rx.State):
+    """The app state."""
+
+    pass
+
+
+def index() -> rx.Component:
+    return rx.container(
+        rx.box(
+            "What is Reflex?",
+            text_align="center",
+        ),
+        rx.box(
+            "A way to build webb apps in pure Python.",
+            text_align="left",
+        ),
+    )
+
+
+# Add state and page to the app.
+app = rx.App()
+app.add_page(index)
+app.compile()
---
+++ rxconfig.py
@@ -0,0 +1,10 @@
+import reflex as rx
+
+class ReflexchatappConfig(rx.Config):
+    pass
+
+config = ReflexchatappConfig(
+    app_name="reflex_chatapp",
+    db_url="sqlite:///reflex.db",
+    env=rx.Env.DEV,
+)
